37334866057500a7b906a5f33355fb42e23c0a5f
Alex Dunne <56739831+dunnesquared@users.noreply.github.com>
2021-08-20 16:17:19 -0400

    Add title to UI

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
 // import './App.css';
 
-import Question from './components/Question/Question';
-import AnswerForm from './components/Answer/AnswerForm';
-import PlayerStatus from './components/PlayerStatus/PlayerStatus';
+import Title from "./components/Title/Title";
+import Question from "./components/Question/Question";
+import AnswerForm from "./components/Answer/AnswerForm";
+import PlayerStatus from "./components/PlayerStatus/PlayerStatus";
 
 function App() {
-  const [question, setQuestion] = useState("");
+  // App title
+  const title = "Ahoj! Welcome to Bad Banana💩🍌!";
 
-  useEffect( () => {
+  const [question, setQuestion] = useState("");
+  const [statusData, setStatusData] = useState("");
+
+  const updatePlayerStatus = (statusData) => {
+    setStatusData(statusData);
+  };
+
+  useEffect(() => {
     // Create a player by default so you don't get errors later on.
-    fetch('/api/question')
-      .then(res => res.json())
-        .then(data => { setQuestion(data.question); });
+    fetch("/api/question")
+      .then((res) => res.json())
+      .then((data) => {
+        setQuestion(data.question);
+      });
   }, []);
 
   return (
     <div>
-      <p>Ahoj!</p>
+      <Title title={title} />
       <Question question={question} />
+      <AnswerForm onUpdatePlayerStatus={updatePlayerStatus} />
     </div>
   );
 }
 
 export default App;
-
