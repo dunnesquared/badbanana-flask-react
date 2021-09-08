f76c87aaef8f89dfada4fb47825a207f109ca51f
Alex Dunne <56739831+dunnesquared@users.noreply.github.com>
2021-09-08 18:36:52 -0400

    Add selection box for question type to UI

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -2,24 +2,46 @@ import React, { useState } from "react";
 
 const Question = (props) => {
   const [question, setQuestion] = useState("...");
+  const [questionType, setQuestionType] = useState("");
 
-  const clickHandler = () => {
-    console.log("click!");
+  const changeHandler = (event) => {
+    setQuestionType(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    console.log("submit!");
+    console.log(questionType);
     fetch("/api/question")
       .then((res) => res.json())
       .then((data) => {
         console.log(data.question);
         setQuestion(data.question);
       });
+    event.preventDefault();
   };
 
   return (
-    <div>
-      <p>What is {question}?</p>
+    <form onSubmit={submitHandler}>
       <div>
-        <button onClick={clickHandler}>Generate Question</button>
+        <label>
+          What kind of arithmetic do you want to practice?
+          <div>
+            <select value={questionType} onChange={changeHandler}>
+              <option value="">--Please choose a question type--</option>
+              <option value="Multiplication">Multiplication</option>
+              <option value="Division">Division</option>
+              <option value="Addition">Addition</option>
+              <option value="Subtraction">Subtraction</option>
+            </select>
+          </div>
+        </label>
+        <div>
+          <br></br>
+          <input type="submit" value="Generate Question" />
+        </div>
       </div>
-    </div>
+      <p>What is {question}?</p>
+    </form>
   );
 };
 
