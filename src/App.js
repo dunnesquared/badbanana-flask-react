296203648d457b850bfa3f784bd2c592d3053402
Alex Dunne <56739831+dunnesquared@users.noreply.github.com>
2021-11-12 15:49:17 -0500

    Remove unused prop function

--- src/App.js
+++ src/App.js
@@ -62,7 +62,6 @@ function App() {
       <AnswerForm
         onUpdateGameState={updateGameState}
         newGame={newGame}
-        onUpdateNewGameToFalse={updateNewGameToFalse}
       />
       <AnswerResult
         answerCorrect={answerCorrect}
