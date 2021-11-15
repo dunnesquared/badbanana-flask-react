f127edf3cde443ceb3f2c4a80dc821c439958325
Alex Dunne <56739831+dunnesquared@users.noreply.github.com>
2021-11-15 15:43:18 -0500

    Add punit tests to check start state of score and lives

--- api/test_api.py
+++ api/test_api.py
@@ -2,6 +2,7 @@ import pytest
 
 from api import create_app
 
+
 @pytest.fixture
 def client():
     app = create_app({'TESTING': True})
@@ -17,3 +18,15 @@ def test_hello(client):
     rv = client.get('/api/hello')
     json_data = rv.get_json()
     assert "hello, world" == json_data['msg']
+
+
+def test_newgame(client):
+    rv = client.get('/api/new-game', follow_redirects=True)
+    json_data = rv.get_json()
+    assert json_data['score'] == 0 and json_data['lives'] == 3
+
+
+def test_get_score_lives_newgame(client):
+    rv = client.get('api/score-lives', follow_redirects=True)
+    json_data = rv.get_json()
+    assert json_data['score'] == 0 and json_data['lives'] == 3
