031debf86b6b7b26b946ee5cb6475e1be8559591
Alex Dunne <56739831+dunnesquared@users.noreply.github.com>
2021-11-16 15:32:52 -0500

    Test  generate_question view with success data

--- api/test_api.py
+++ api/test_api.py
@@ -30,3 +30,46 @@ def test_score_lives_init(client):
     rv = client.get('api/score-lives', follow_redirects=True)
     json_data = rv.get_json()
     assert json_data['score'] == 0 and json_data['lives'] == 3
+
+def test_generate_question_good_data(client):
+    """
+    question_type = request_data['questionType']
+    lowerbound = int(request_data['smallestNumber'])
+    upperbound = int(request_data['largestNumber'])
+    """
+    qtype = 'Multiplication'
+    nsmallest = 1
+    nlargest = 1000
+    payload = {
+        'questionType': qtype,
+        'smallestNumber': nsmallest,
+        'largestNumber': nlargest,
+    }
+    rv = client.post('/api/question', json=payload)
+    json_data = rv.get_json()
+
+    # Test success
+    assert json_data['success'] == True
+
+    # Test existence of data,
+    assert 'question' in json_data
+    assert 'operand1' in json_data
+    assert 'operand2' in json_data
+    assert 'operator' in json_data
+    assert 'answer' in json_data
+
+    # Test value bounds.
+    assert nsmallest <= json_data['operand1'] <= nlargest
+    assert nsmallest <= json_data['operand2'] <= nlargest
+
+    # Test whether operator is valid.
+    assert json_data['operator'] in {'+', '*', '/', '-'}
+
+    # Test whether provided answer is correct
+    assert json_data['answer'] == eval(json_data['question'])
+
+
+
+    
+
+
